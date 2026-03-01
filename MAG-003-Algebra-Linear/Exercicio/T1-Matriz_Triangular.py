80f895d3ce431cf9d95ccd70e67efaf71017c4e2
WellingtonTorres <wellingtontorresmelo@gmail.com>
2026-03-01 18:34:48 -0300

    SOBE PARTE DO EXERCII

---
+++ MAG-003-Algebra-Linear/Exercicio/T1-Matriz_Triangular.py
@@ -0,0 +1,62 @@
+# T1- Matriz Triangular
+# Faça um programa em Python que:
+# Peça ao usuário para digitar todos os elementos uma matriz A45
+# * Deve retornar:
+#  - Matriz A
+#  - Matriz A Superior
+#  - Matriz B Inferior
+# QUESTIONAR A PROFESSORA SOBRE A45, POIS NAO HA MATRIZ TRIANGULAR REAL SE NAO FOR QUADRATICA
+
+#Digite os elementos da Matriz A45:
+
+#func para maior legibilidade do output
+def subscrito(num):
+    sequencia = "0123456789"
+    sub = "₀₁₂₃₄₅₆₇₈₉"
+    return num.translate(str.maketrans(sequencia, sub))
+
+print('Digite os elementos da Matriz Triangular A₄₅:')
+
+nLinhas = 3
+nColunas = 3
+sExibeElemMatriz = ""
+# matriz = [[0] * nColunas] * nLinhas # dessa forma cria referência da lista na mesma lista
+matriz = [[0] * nColunas for _ in range(nLinhas)]
+lSuperior = True
+lInferior = True
+
+
+#apenas verifico
+print(matriz)
+
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        sExibeElemMatriz = str(i + 1) + str(j + 1)
+        sExibeElemMatriz = subscrito(sExibeElemMatriz)
+        matriz[i][j] = int(input(f'A{sExibeElemMatriz}: '))
+        if i > j: #avaliando inferior
+            if matriz[i][j] > 0:
+                lInferior = False
+        if i < j: #avaliando superior
+            if matriz[i][j] > 0:
+                lSuperior = False
+
+
+if lSuperior and not lInferior:
+    print(f'É uma matriz triangular superior!')
+elif lInferior and not lSuperior:
+    print(f'É uma matriz triangular inferior!')
+else:
+    print(f'Não é uma matriz triangular!')
+    
+    
+for i in range(len(matriz)):
+    print(matriz[i])
+    # for j in range(len(matriz[i])):
+        
+        
+
+#     for j in range(nColunas):
+
+
+# print("matriz A" + subscrito(45)) 
